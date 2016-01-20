26586a87d36f2bdef41047cc58078a1392b422f5
jamesliauw <jamesliauw@163.com>
2016-01-20 11:14:35 +0800

    加入NG-ROUTE导航结构
    app.js

--- app/app.js
+++ app/app.js
@@ -3,10 +3,83 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+
+config(['$routeProvider',  function($routeProvider) {
+
+    //$locationProvider.html5Mode(true);
+
+    $routeProvider.when("/labby-hall", {
+        templateUrl: "view/labby-hall.html",
+        controller: "LabbyHallCtrl"
+    });
+
+    $routeProvider.when("/about", {
+        templateUrl: "view/about.html",
+        controller: "AboutCtrl"
+    });
+
+    $routeProvider.when("/cart", {
+        templateUrl: "view/cart.html",
+        controller: "CartCtrl"
+    });
+
+    $routeProvider.when("/contact", {
+        templateUrl: "view/contact.html",
+        controller: "ContactCtrl"
+    });
+
+    $routeProvider.when("/social", {
+        templateUrl: "view/social.html",
+        controller: "SocialCtrl"
+    });
+
+    $routeProvider.when("/user", {
+        templateUrl: "view/user.html",
+        controller: "UserCtrl"
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/labby-hall'
+    });
+}])
+
+
+.controller("defaultCtrl", function ($scope, $location) {
+
+    $scope.data = {};
+
+    //$scope.createProduct = function (product) {
+    //    new productsResource(product).$create().then(function (newProduct) {
+    //        $scope.data.products.push(newProduct);
+    $location.path("/");
+    //    });
+    //}
+
+    //$scope.deleteProduct = function (product) {
+    //    product.$delete().then(function () {
+    //        $scope.data.products.splice($scope.data.products.indexOf(product), 1);
+    //    });
+
+    $location.path("/");
+    //}
+})
+.controller("LabbyHallCtrl", function ($scope, $location, $route) {
+
+})
+.controller("AboutCtrl", function ($scope, $location, $route) {
+
+})
+.controller("CartCtrl", function ($scope, $location, $route) {
+
+})
+.controller("ContactCtrl", function ($scope, $location, $route) {
+
+})
+.controller("SocialCtrl", function ($scope, $location, $route) {
+
+})
+.controller("UserCtrl", function ($scope, $location, $route) {
+
+})
