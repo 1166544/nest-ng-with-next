deaaa9bf5e73341ea4d279cc4a30caa159a69f8f
James Liauw <jamesliauw@163.com>
2020-03-29 11:21:47 +0800

    feat: add docs

--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -1,28 +1,56 @@
 import React from 'react';
 
+/**
+ * 服务端传给页面参数结构
+ *
+ * @interface InitialProps
+ */
 interface InitialProps {
   query: any;
 }
 
+/**
+ * 页面参数细分结构
+ *
+ * @interface Props
+ * @extends {InitialProps}
+ */
 interface Props extends InitialProps {
   title: string;
   content: string;
 }
 
+/**
+ * 渲染页面
+ *
+ * @class Index
+ * @extends {React.Component<Props>}
+ */
 class Index extends React.Component<Props> {
-  public static getInitialProps({ query }: InitialProps) {
-    // tslint:disable-next-line: no-console
-    // console.log('query...', query);
 
-    return query;
-  }
+	/**
+	 * 获取渲染页面数据
+	 *
+	 * @static
+	 * @param {InitialProps} { query }
+	 * @returns
+	 * @memberof Index
+	 */
+	public static getInitialProps({ query }: InitialProps) {
+		return query;
+	}
 
-  public render() {
-    // tslint:disable-next-line: no-console
-    console.log('query...', this.props);
+	/**
+	 * 渲染
+	 *
+	 * @returns
+	 * @memberof Index
+	 */
+	public render() {
+		console.log('pageData...', this.props);
 
-    return (<div><a>{this.props.title}</a></div>);
-  }
+		return (<div><a>{this.props.title}</a></div>);
+	}
 }
 
 export default Index;
