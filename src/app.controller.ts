e83a7cdb3818d073042dfd91af3714d27247a8dc
James Liauw <jamesliauw@163.com>
2020-03-29 11:41:32 +0800

    feat: add alloy tslint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,32 @@
 import {
-  CacheInterceptor,
-  Controller,
-  Get,
-  Render,
-  UseInterceptors,
+	CacheInterceptor,
+	Controller,
+	Get,
+	Render,
+	UseInterceptors
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * 顶层控制器
+ *
+ * @export
+ * @class AppController
+ */
 @Controller()
 @UseInterceptors(CacheInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('Index')
-  public root(): any {
-    return this.appService.root();
-  }
+	/**
+	 * 首页入口
+	 *
+	 * @returns {*}
+	 * @memberof AppController
+	 */
+	@Get()
+	@Render('Index')
+	public root(): any {
+		return this.appService.root();
+	}
 }
--- src/cache/cache-config.service.ts
+++ src/cache/cache-config.service.ts
@@ -1,36 +1,74 @@
 import {
-  CacheModuleOptions,
-  CacheOptionsFactory,
-  Injectable,
+	CacheModuleOptions,
+	CacheOptionsFactory,
+	Injectable
 } from '@nestjs/common';
 import { ConfigService } from '@server/config/config.service';
 
+/**
+ * 缓存服务
+ *
+ * @export
+ * @class CacheConfigService
+ * @implements {CacheOptionsFactory}
+ */
 @Injectable()
 export class CacheConfigService implements CacheOptionsFactory {
-  constructor(private configService: ConfigService) {}
 
-  /**
-   * Example retry strategy for when redis is used for the cache
-   * This example is only compatible with cache-manager-redis-store because it used node_redis
-   */
-  public retryStrategy() {
-    return {
-      retry_strategy: (options: any) => {
-        if (options.error && options.error.code === 'ECONNREFUSED') {
-          return new Error('The server refused the connection');
-        }
-        if (options.total_retry_time > 1000 * 60) {
-          return new Error('Retry time exhausted');
-        }
-        if (options.attempt > 2) {
-          return new Error('Max attempts exhausted');
-        }
-        return Math.min(options.attempt * 100, 3000);
-      },
-    };
-  }
+	/**
+	 * 等待时间
+	 *
+	 * @private
+	 * @type {number}
+	 * @memberof CacheConfigService
+	 */
+	private waitTime: number = 3000;
 
-  public createCacheOptions(): CacheModuleOptions {
-    return {};
-  }
+	/**
+	 * 1分钟等待时间
+	 *
+	 * @private
+	 * @type {number}
+	 * @memberof CacheConfigService
+	 */
+	private waitMinuteTime: number = 60;
+
+	constructor(private configService: ConfigService) {
+		// hole
+	}
+
+	/**
+	 * Example retry strategy for when redis is used for the cache
+	 * This example is only compatible with cache-manager-redis-store because it used node_redis
+	 *
+	 * @returns
+	 * @memberof CacheConfigService
+	 */
+	public retryStrategy(): any {
+		return {
+			retry_strategy: (options: any): any => {
+				if (options.error && options.error.code === 'ECONNREFUSED') {
+					return new Error('The server refused the connection');
+				}
+				if (options.total_retry_time > 1000 * this.waitMinuteTime) {
+					return new Error('Retry time exhausted');
+				}
+				if (options.attempt > 2) {
+					return new Error('Max attempts exhausted');
+				}
+
+				return Math.min(options.attempt * 100, this.waitTime);
+			}
+		};
+	}
+
+	/**
+	 * 创建缓存选项
+	 *
+	 * @returns {CacheModuleOptions}
+	 * @memberof CacheConfigService
+	 */
+	public createCacheOptions(): CacheModuleOptions {
+		return {};
+	}
 }
