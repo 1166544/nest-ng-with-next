5a298d72c22961815d63ae665d3c489f8bf68742
James Liauw <jamesliauw@163.com>
2020-03-29 11:06:14 +0800

    feat: update alll old files

--- README.md
+++
@@ -1,5 +0,0 @@
-# TotallySell Admin AngularJS single page apps
-安装依赖        npm install
-启动            npm start
-访问            http://localhost:8000/app/index.html
-自动更新        gulp watch
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var liveReload = require('gulp-livereload');
-
-gulp.task('watch', function(){
-   var server = liveReload();
-    gulp.watch('app/**/*.*', function(file){
-       server.change(file.path);
-    });
-});
