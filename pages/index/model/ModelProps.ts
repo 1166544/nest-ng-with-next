248c030872685ff001226662663c8d49cbd71ab0
James Liauw <jamesliauw@163.com>
2020-03-30 09:46:38 +0800

    feat: add common

---
+++ pages/index/model/ModelProps.ts
@@ -0,0 +1,12 @@
+import { IInitialProps } from '@src/model/ModelInitialProps';
+
+/**
+ * 页面参数细分结构
+ *
+ * @interface Props
+ * @extends {IInitialProps}
+ */
+export interface IProps extends IInitialProps {
+	title: string;
+	content: string;
+}
