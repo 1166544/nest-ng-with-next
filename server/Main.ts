320c1d213808c6d3f0c0566ba04f6a4a6db4d0a6
James Liauw <jamesliauw@163.com>
2020-04-07 19:58:27 +0800

    feat: add security

--- server/Main.ts
+++ server/Main.ts
@@ -1,13 +1,28 @@
 import dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
+import Consola from 'consola';
+import configParser from './logics/common/config/ConfigParser';
 import { AppModule } from './AppModule';
 import { LogicModule } from './logics/LogicModule';
 import { NextModule } from './logics/next/NextModule';
+import { ConfigDefault } from './config/ConfigDefault';
 
 dotenv.config();
 
-(async (): Promise<any> => {
+/**
+ * bootstrap
+ *
+ * @returns {Promise<any>}
+ */
+async function bootstrap(): Promise<any> {
+	const configer: ConfigDefault = configParser.getConfig();
+	const appPort: string = process.env.APP_PORT;
+	const appHost: string = process.env.APP_HOST;
+	const appProtocol: string = process.env.APP_PROTOCOL;
 	const app: any = await NestFactory.create<NestExpressApplication>(
 		AppModule
 	);
@@ -15,13 +30,22 @@ dotenv.config();
 	app.useStaticAssets('public');
 	app.get(LogicModule).initialize(app);
 
+	// security
+	app.use(helmet());
+	app.enableCors();
+	app.use(cookieParser());
+	app.use(rateLimit(configer.getRateConfig()));
+
 	app.get(NextModule)
-		.prepare()
-		.then((): any => {
-			app.listen(process.env.APP_PORT, process.env.APP_HOST, (): any => {
-				console.log(
-					`[ Apollo ] Ready on ${process.env.APP_PROTOCOL}://${process.env.APP_HOST}:${process.env.APP_PORT}`
-				);
+	.prepare()
+	.then((): any => {
+		app.listen(appPort, appHost, (): any => {
+			Consola.ready({
+				message: `[ Apollo ] successfully started on ${appProtocol}://${appHost}:${appPort}`,
+				badge: true
 			});
 		});
-})();
+	});
+}
+
+bootstrap();
