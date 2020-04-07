5dcfca1aa3bd6151fe0c0e05685650b2cff703cc
James Liauw <jamesliauw@163.com>
2020-04-07 09:06:17 +0800

    feat: refact for rules class

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,59 +4,105 @@ import { Typography, Button, Card, CardContent } from '@material-ui/core';
 import { Http } from '../lib/http';
 import { Layout } from '../components/Layout';
 
-const useStyles: any = makeStyles((theme: Theme): any =>
-	createStyles({
-		root: {
-			textAlign: 'center',
-			paddingTop: theme.spacing(8)
-		},
-		container: {
-			width: 480,
-			margin: `${theme.spacing(2)}px auto`
-		},
-		card: {
-			padding: theme.spacing(4)
-		}
-	})
-);
+/**
+ * index page
+ *
+ * @class Index
+ * @extends {React.Component<any>}
+ */
+class Index extends React.Component<any> {
+	public classes: any;
 
-const Index: any = ({ user }): any => {
-	const classes: any = useStyles({});
+	constructor(props: any, context?: any) {
+		super(props, context);
+		this.classes = this.useStyles({});
+	}
 
-	const onClick: any = async (e: React.MouseEvent): Promise<any> => {
+	/**
+	 * use styles
+	 *
+	 * @private
+	 * @param {*} themeData
+	 * @returns {*}
+	 * @memberof Index
+	 */
+	private useStyles(themeData: any): any {
+		return makeStyles((theme: Theme): any =>
+			createStyles({
+				root: {
+					textAlign: 'center',
+					paddingTop: theme.spacing(8)
+				},
+				container: {
+					width: 480,
+					margin: `${theme.spacing(2)}px auto`
+				},
+				card: {
+					padding: theme.spacing(4)
+				}
+			})
+		);
+	}
+
+	/**
+	 * 获取渲染页面数据
+	 *
+	 * @static
+	 * @param {*} { req }
+	 * @returns {Promise<any>}
+	 * @memberof Index
+	 */
+	public static async getInitialProps({ req }: any): Promise<any> {
+		const { user } = req;
+
+		return {
+			user
+		};
+	}
+
+	/**
+	 * on click
+	 *
+	 * @private
+	 * @param {React.MouseEvent} e
+	 * @returns {Promise<any>}
+	 * @memberof Index
+	 */
+	private async onClick(e: React.MouseEvent): Promise<any> {
 		e.preventDefault();
 		const http: Http = new Http();
 		const response: any = await http.post('api/auth/logout');
 		if (response.ok) {
 			location.href = '/auth/login';
 		}
-	};
+	}
 
-	return (
-		<Layout>
-			<div className={classes.root}>
-				<div className={classes.container}>
-					<Card className={classes.card}>
-						<CardContent>
-							<Typography variant="body1">You are now logged in as {user.name} :)</Typography>
-							<br />
-							<Button type="submit" variant="outlined" color="primary" size="large" onClick={onClick}>
-								LOGOUT
-							</Button>
-						</CardContent>
-					</Card>
+	/**
+	 * render
+	 *
+	 * @returns {*}
+	 * @memberof Index
+	 */
+	public render(): any {
+		return (
+			<Layout>
+				<div className={this.classes.root}>
+					<div className={this.classes.container}>
+						<Card className={this.classes.card}>
+							<CardContent>
+								<Typography variant="body1">You are now logged in as {this.props.user.name} :)</Typography>
+								<br />
+								<Button type="submit" variant="outlined" color="primary" size="large" onClick={this.onClick.bind(this)}>
+									LOGOUT
+								</Button>
+							</CardContent>
+						</Card>
+					</div>
 				</div>
-			</div>
-		</Layout>
-	);
-};
+			</Layout>
+		);
+	}
 
-Index.getInitialProps = async ({ req }): Promise<any> => {
-	const { user } = req;
-
-	return {
-		user
-	};
-};
+}
 
 export default Index;
